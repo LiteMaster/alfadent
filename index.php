e774ae0689b0a7a173b2d33369a0beffe0a29f51
Valeriy Scherbakov <it@agatech.ru>
2024-05-06 17:56:08 +0400

    Version 1

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'templates/head.php';
+require_once 'templates/header.php';
+require_once 'templates/content.php';
+require_once 'templates/footer.php';
+require_once 'templates/bottom.php';
---
+++ templates/bottom.php
@@ -0,0 +1,3 @@
+<script src="js/main.sj"></script>
+</body>
+</html>
